4642faf75d7b3c6cf0f465ef5123d6126ecf98f0
Jeremie Abiteboul <abiteboul@spideo.tv>
2023-11-23 12:47:43 +0100

    mock python requests lib to test API calls

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -18,15 +18,52 @@ def test_base_url(url, source, expected):
 @pytest.mark.parametrize(
     "endpoint, expected",
     [
-        ("/a/b/c", "url/mysource/a/b/c"),
-        ("a/b/c", "url/mysource/a/b/c"),
+        ("/a/b/c", "url/source/a/b/c"),
+        ("a/b/c", "url/source/a/b/c"),
     ],
 )
 def test_generate_url(endpoint, expected):
-    api_client = RumoClient("url", "mysource", "")
+    api_client = RumoClient("url", "source", "")
     assert api_client._generate_url(endpoint) == expected
 
 
 def test_api_key_in_headers():
-    api_client = RumoClient("url", "mysource", "key")
+    api_client = RumoClient("url", "source", "key")
     assert api_client._base_headers == {"x-api-key": "key"}
+
+
+def test_call_api(requests_mock):
+    requests_mock.get("https://rumo.com/source/endpoint", json={})
+    api_client = RumoClient("https://rumo.com", "source", "key")
+    api_client.call_api("GET", "endpoint")
+    assert requests_mock.call_count == 1
+    request = requests_mock.last_request
+    assert request.method == "GET"
+    assert request.url == "https://rumo.com/source/endpoint"
+    assert request.path == "/source/endpoint"
+    assert {"x-api-key": "key"}.items() <= request.headers.items()
+
+
+def test_call_api_query_params(requests_mock):
+    requests_mock.get("https://rumo.com/source/endpoint", json={})
+    api_client = RumoClient("https://rumo.com", "source", "key")
+    query_params = {"key1": "value1", "key2": "value2"}
+    api_client.call_api("GET", "endpoint", query_params=query_params)
+    assert requests_mock.call_count == 1
+    request = requests_mock.last_request
+    assert request.url in [
+        "https://rumo.com/source/endpoint?key1=value1&key2=value2",
+        "https://rumo.com/source/endpoint?key2=value2&key1=value1",
+    ]
+
+
+def test_call_api_headers(requests_mock):
+    requests_mock.get("https://rumo.com/source/endpoint", json={})
+    api_client = RumoClient("https://rumo.com", "source", "key")
+    header_params = {"key1": "value1", "key2": "value2"}
+    expected_headers = {"key1": "value1", "key2": "value2", "x-api-key": "key"}
+    api_client.call_api("GET", "endpoint", header_params=header_params)
+    assert requests_mock.call_count == 1
+    request = requests_mock.last_request
+    assert request.url == "https://rumo.com/source/endpoint"
+    assert expected_headers.items() <= request.headers.items()
