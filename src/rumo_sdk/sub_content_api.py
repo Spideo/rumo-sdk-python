da8b6e565305f2e529bbc909ea4fefa55ee60409
Jeremie Abiteboul <abiteboul@spideo.tv>
2024-01-30 12:30:16 +0100

    add endpoints for sub-contents

---
+++ src/rumo_sdk/sub_content_api.py
@@ -0,0 +1,35 @@
+from typing import Optional, TypedDict
+
+from rumo_sdk.content_api import ContentApi
+from rumo_sdk.utils import batched
+
+
+class SubContents(TypedDict):
+    contentId: str
+    subContentIds: list[str]
+
+
+class SubContentApi(ContentApi):
+    def get_sub_contents(self, content_id: str) -> dict:
+        """https://apidoc.rumo.co/#get-/content/-contentId-/sub-contents"""
+        endpoint = f"/content/{content_id}/sub-contents"
+        return self._rumo_client.get(endpoint)
+
+    def _add_sub_contents(self, list_of_relations: list[SubContents]) -> dict:
+        """https://apidoc.rumo.co/#post-/content/sub-contents"""
+        endpoint = "/content/sub-contents"
+        return self._rumo_client.post(endpoint, json=list_of_relations)
+
+    def add_sub_contents(
+        self, list_of_relations: list[SubContents], batch_size: Optional[int] = 100
+    ):
+        """https://apidoc.rumo.co/#post-/content/sub-contents"""
+        for batch in batched(list_of_relations, batch_size):
+            print(f"\nSending batch of {len(batch)} sub-content relations to Rumo API.")
+            self._add_sub_contents(batch)
+        print("\nFinished uploading sub-contents.")
+
+    def delete_sub_contents(self, content_id: str) -> dict:
+        """https://apidoc.rumo.co/#get-/content/-contentId-/sub-contents"""
+        endpoint = f"/content/{content_id}/sub-contents"
+        return self._rumo_client.delete(endpoint)
