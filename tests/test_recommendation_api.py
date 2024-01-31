679b6949a7f1d845a90cb326091e6fac60c8a1ec
Jeremie Abiteboul <abiteboul@spideo.tv>
2024-01-31 15:46:52 +0100

    Minor test refacto to avoid duplicate code

--- tests/test_recommendation_api.py
+++ tests/test_recommendation_api.py
@@ -1,16 +1,4 @@
-from unittest.mock import MagicMock
-
-import pytest
-
-from rumo_sdk.api_client import RumoClient
 from rumo_sdk.recommendation_api import RecommendationApi
-
-
-@pytest.fixture
-def api_mock():
-    api_mock = RumoClient("url", "source", "key")
-    api_mock.call_api = MagicMock()
-    return api_mock
 
 
 def test_similar(api_mock):
--- tests/test_users_api.py
+++ tests/test_users_api.py
@@ -1,16 +1,6 @@
-from unittest.mock import MagicMock
-
 import pytest
 
-from rumo_sdk.api_client import RumoClient
 from rumo_sdk.users_api import UsersApi
-
-
-@pytest.fixture
-def api_mock():
-    api_mock = RumoClient("url", "source", "key")
-    api_mock.call_api = MagicMock()
-    return api_mock
 
 
 def test_get_user_choice(api_mock):
