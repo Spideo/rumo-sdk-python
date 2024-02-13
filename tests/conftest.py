cbe7b329f5b553e1e5db6ed973f75f1f53080272
Jeremie Abiteboul <abiteboul@spideo.tv>
2024-02-13 14:18:37 +0100

    minor tweaks for usability
    validation by default + kwargs in POST

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,11 @@ from unittest.mock import MagicMock
 import pytest
 
 from rumo_sdk.api_client import RumoClient
+from rumo_sdk.open_api import OpenApiType
 
 
 @pytest.fixture
 def api_mock():
-    api_mock = RumoClient("url", "source", "key")
+    api_mock = RumoClient("url", "source", "key", openapi_type=OpenApiType.NONE)
     api_mock.call_api = MagicMock()
     return api_mock
