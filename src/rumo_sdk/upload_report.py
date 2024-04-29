bb9a9873b80954ee1aafc996cc1b4fc777e6a3d3
araux1 <raux@spideo.tv>
2024-04-29 12:09:49 +0200

    cr1

--- src/rumo_sdk/upload_report.py
+++ src/rumo_sdk/upload_report.py
@@ -1,4 +1,5 @@
 import re
+from typing import Optional
 
 
 class ValidationError:
@@ -7,6 +8,13 @@ class ValidationError:
         self.message = message
         self.value = value
         self.errorType = errorType
+
+    def parse_id_from_error_message(self) -> Optional[str]:
+        matches = re.findall(r"\[(\d+)\]", self.message)
+        if matches:
+            return matches[0]
+        else:
+            None
 
 
 class UploadReport:
@@ -23,24 +31,27 @@ class UploadReport:
     @classmethod
     def build_report(cls, catalog, upload_responses):
         processed_content = cls.get_processed_content_ids(catalog)
-        validation_errors = cls.get_validation_errors(cls, upload_responses)
-        invalid_content = cls.get_invalid_content(cls, validation_errors)
+        validation_errors = cls.get_validation_errors(upload_responses)
+        invalid_content = cls.get_invalid_content(validation_errors)
         return cls.__init__(cls, processed_content, invalid_content, validation_errors)
 
+    @staticmethod
     def get_processed_content_ids(catalog) -> list[str]:
         processed_content_ids = []
         for content in catalog:
-            if content["id"]:
+            if "id" in content:
                 processed_content_ids.append(content["id"])
         return processed_content_ids
 
+    @classmethod
     def get_validation_errors(cls, upload_responses) -> list[ValidationError]:
         validation_errors = []
         for response in upload_responses:
             cls.build_validation_errors(response, validation_errors)
         return validation_errors
 
-    def build_validation_errors(response, validation_errors) -> ValidationError:
+    @staticmethod
+    def build_validation_errors(response, validation_errors):
         if response["validationErrors"] and len(response["validationErrors"]) != 0:
             for error in response["validationErrors"]:
                 errorCode = error["errorCode"]
@@ -51,12 +62,11 @@ class UploadReport:
                     ValidationError(errorCode, message, value, errorType)
                 )
 
-    def get_invalid_content(cls, validation_errors: list[ValidationError]) -> list[str]:
+    @staticmethod
+    def get_invalid_content(validation_errors: list[ValidationError]) -> list[str]:
         invalid_content = []
         for error in validation_errors:
-            content_id = cls.parse_id_from_error_message(error.message)
-            invalid_content.append(content_id)
+            content_id = error.parse_id_from_error_message()
+            if content_id:
+                invalid_content.append(content_id)
         return invalid_content
-
-    def parse_id_from_error_message(message: str) -> str:
-        return re.findall(r"\[(\d+)\]", message)
