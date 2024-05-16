d05b663a5f28e5bec1bd74a2d5a1d905b15c391b
araux1 <raux@spideo.tv>
2024-05-16 11:30:13 +0200

    format

--- tests/test_upload_report.py
+++ tests/test_upload_report.py
@@ -77,17 +77,17 @@ def test_build_report_when_subcontents_error():
 
 def test_validation_error_eq():
     err1 = ValidationError(
-            "ERROR_0204",
-            "Content not Found: contentId does not exist.",
-            "2",
-            "RequestBodyValidation",
-        )
+        "ERROR_0204",
+        "Content not Found: contentId does not exist.",
+        "2",
+        "RequestBodyValidation",
+    )
     err2 = ValidationError(
-            "ERROR_0204",
-            "Content not Found: contentId does not exist.",
-            "2",
-            "RequestBodyValidation",
-        )
+        "ERROR_0204",
+        "Content not Found: contentId does not exist.",
+        "2",
+        "RequestBodyValidation",
+    )
     assert err1.__eq__(err2).__eq__(True)
 
 
